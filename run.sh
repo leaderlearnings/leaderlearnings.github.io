b912a24d1883c4b3b69650a249d22392bb041d02
Rahul Yadav <ryconnect@gmail.com>
2025-02-27 01:00:34 -0500

    fix icons and other metadata

---
+++ run.sh
@@ -0,0 +1,4 @@
+PATH=/home/stranger/gems/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin:$PATH
+export PATH
+echo $PATH
+jekyll serve
